fb298e171fb6a4f432fbd42f1a90bb649bb4754a
beratridvanasilturk <br.asltrk@gmail.com>
2023-07-11 17:50:18 +0300

    Added Animation with CATransition

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -28,46 +28,59 @@ class ViewController: UIViewController {
     }
     
     func startNewRound(){
+        //CATransition Animation
+        let transition = CATransition()
+        // Transition type new round baslerken bu class icerisindeki propertyleri anime etme seklini belirliyor. Dikkatli bakilacak olursa hem slider'i hem de label'lari .fade animasyonu ile guncellendigi gorulur.
+        transition.type = .fade
+        transition.duration = 0.3
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
+        
         targetValue = Int.random(in: 1...100)
         currentValue = 50
         slider.value = Float(currentValue)
         updateLabels()
     }
-
+    
     func updateLabels(){
+        
         targetLbl.text = String(targetValue)
         scoreLbl.text = String(totalScore)
         roundLbl.text = String(round)
+        
     }
-
+    
     //MARK: Actions
     @IBAction func startOverButtonTapped(_ sender: UIButton) {
         
-            hitMeButtonOutlet.isEnabled = true
-            round = 0
-            totalScore = 0
-            currentValue = 0
-            startNewRound()
+        hitMeButtonOutlet.isEnabled = true
+        round = 0
+        totalScore = 0
+        currentValue = 0
+        startNewRound()
+        
+        let transition = CATransition()
+        // Transition type oyun tekrar baslerken bu class icerisindeki propertyleri anime etme seklini belirliyor.
+        transition.type = .push
+        transition.duration = 0.8
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
         
     }
     @IBAction func infoButtonTapped(_ sender: UIButton) {
-        
     }
+    
     @IBAction func sliderMoved(_ sender: UISlider) {
         
         currentValue = lroundf(sender.value)
+        
     }
     
     @IBAction func hitMeButtonTapped(_ slider: UIButton) {
         
-        
         //Difference Calculator, abs mean is absolute value (mutlak deger)
         let differenceTargetCurrent = abs(currentValue - targetValue)
-        
         var points = 100 - differenceTargetCurrent
-        
-        
-        
         var title: String
         
         if differenceTargetCurrent == 0 {
@@ -88,7 +101,7 @@ class ViewController: UIViewController {
         "\n Your guess \(currentValue)" +
         "\n Total score is \(totalScore)"
         
-        var alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "OK", style: .default, handler: { _ in
             self.startNewRound()
@@ -108,8 +121,6 @@ class ViewController: UIViewController {
             alert.addAction(action)
             
         }
-    
-        
     }
 }
 
